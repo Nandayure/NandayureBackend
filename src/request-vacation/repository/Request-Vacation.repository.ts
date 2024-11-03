e4a4c16b0c532be7224422fda6115cf636851d00
Marco Cortes <marcortes.stiven@gmail.com>
2024-11-03 17:23:12 -0600

    change import

--- src/request-vacation/repository/Request-Vacation.repository.ts
+++ src/request-vacation/repository/Request-Vacation.repository.ts
@@ -2,7 +2,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { RequestVacation } from '../entities/request-vacation.entity';
 import { BaseAbstractRepostitory } from 'src/core/generic-repository/repository/base.repository';
-import { RequestVacationRepositoryInterface } from './request-vacation.interface';
+import { RequestVacationRepositoryInterface } from './request-Vacation.interface';
 
 export class RequestVacationRepository
   extends BaseAbstractRepostitory<RequestVacation>

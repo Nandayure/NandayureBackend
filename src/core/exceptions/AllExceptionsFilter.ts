57f33f313748da5b06fd98b52114d784b658396b
Marco Cortes <marcortes.stiven@gmail.com>
2025-03-03 21:53:27 -0600

    asfd

--- src/core/exceptions/AllExceptionsFilter.ts
+++ src/core/exceptions/AllExceptionsFilter.ts
@@ -14,13 +14,25 @@ export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost): void {
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
-    const httpStatus =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    let httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Ocurrió un error inesperado';
+
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+      message =
+        typeof exceptionResponse === 'object' && exceptionResponse !== null
+          ? (exceptionResponse as any).message
+          : exceptionResponse;
+    } else if (exception instanceof Error) {
+      message = exception.message; // Agrega el mensaje del error real
+      console.error('🔥 Error no manejado:', exception); // Imprime el error en la consola
+    }
 
     const responseBody = {
       statusCode: httpStatus,
+      message,
       timestamp: new Date().toISOString(),
       path: httpAdapter.getRequestUrl(ctx.getRequest()),
     };

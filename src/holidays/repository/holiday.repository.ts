289b2fc1688fa4326c8fb876f793e6f6cbe644f6
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-03-29 10:41:31 -0600

    add holidays module with CRUD operations and DTOs

---
+++ src/holidays/repository/holiday.repository.ts
@@ -0,0 +1,16 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Holiday } from '../entities/holiday.entity';
+import { BaseAbstractRepostitory } from 'src/core/generic-repository/repository/base.repository';
+import { HolidayRepositoryInterface } from './holiday.interface';
+
+export class HolidayRepository
+  extends BaseAbstractRepostitory<Holiday>
+  implements HolidayRepositoryInterface {
+  constructor(
+    @InjectRepository(Holiday)
+    private readonly holidayGenericRepository: Repository<Holiday>,
+  ) {
+    super(holidayGenericRepository);
+  }
+}
